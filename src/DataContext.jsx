a9758265cde9218893208308423ab9c9fc6bb03d
AndresR <83485255+jabali04@users.noreply.github.com>
2023-02-01 12:23:05 -0600

    first commit

--- src/DataContext.jsx
+++ src/DataContext.jsx
@@ -11,7 +11,7 @@ export const DataProvider = ({children}) => {
 
     const [seconds, setSeconds] = useState(0);
     const [isTimeRunning, setIsTimeRunning] = useState(false);
-    const [timeArray, setTimeArray] = useState([60,30,45]); 
+    const [timeArray, setTimeArray] = useState([180,60,120]); 
     const [isLight, setIsLight] = useState(true)
     const timerId = useRef();
 
